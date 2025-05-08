049b92650f060bf427c024d672f5c44f19bac8e1
Alina Karpenko <vt222_kas@student.ztu.edu.ua>
2025-05-08 14:20:03 +0300

    created auth for password based and upd some files

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,7 +2,7 @@
 
 import { NextResponse } from "next/server";
 import User from "../../../models/User"
-import bcrypt from 'bcryptjs'
+//import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -12,7 +12,7 @@ export async function POST(request) {
         const body = await request.json();
         const { login, password } = body;
         const user = await User.findOne({ where: { login } });
-        if(!user || !(await bcrypt.compare(password, user.password))){
+        if(!user || password !== user.password){
             console.log('Неправильна пошта або пароль')
             return NextResponse.json(
                 {message : 'Неправильна пошта або пароль'},
--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,20 +1,44 @@
 'use client'
-
+import { useState } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Login() {
   const { data: session, status } = useSession()
 
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+          { 
+              login, 
+              password 
+          }
+      ),
+    })
+    const data = await res.json();
+    if (res.ok) {
+      window.location.href = '/signin/result';
+    } else {
+      setLogin('');
+      setPassword('');
+    }
+  }
   if (!session) {
     return (
       <div>
         <p>Ви не увійшли в систему</p>
-        <form>
+        <form onSubmit={handleSubmit}>
 <div>
-    <input id="email" type="text" name="email" placeholder="Enter email"/>
+    <input id="login" type="text" name="login" placeholder="Enter login" value={login} onChange={(e) => setLogin(e.target.value)}/>
     </div>
     <div>
-    <input id="password" type="password" name="password" placeholder="Enter password"/>
+    <input id="password" type="password" name="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)}/>
     </div>
     <div>
     <input type="submit" id="register" value="Увійти"/>
